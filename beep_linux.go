47e1b19c7997d75e6441667d72e894c0b9bd7a81
Milan Nikolic <gen2brain@gmail.com>
2017-12-11 17:54:39 +0100

    Add newline

--- beep_linux.go
+++ beep_linux.go
@@ -53,6 +53,7 @@ func ioctl(fd, name, data uintptr) error {
 //
 // On Linux it needs permission to access `/dev/tty0` or `/dev/input/by-path/platform-pcspkr-event-spkr`, and `pcspkr` module must be loaded.
 // User must be in correct groups, usually `input` and/or `tty`.
+//
 // If it can not open device files, it will fallback to sending Bell character (https://en.wikipedia.org/wiki/Bell_character).
 // For bell character in X11 terminals you can enable bell with `xset b on`. For console check `setterm` and `--blength` or `--bfreq` options.
 //
