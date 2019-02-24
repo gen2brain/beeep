0b6e917675014714bc7df63d6338320657ceb3aa
Milan Nikolic <gen2brain@gmail.com>
2019-02-24 18:07:56 +0100

    Show alert if Windows version is not 10

--- alert_windows.go
+++ alert_windows.go
@@ -8,7 +8,14 @@ import (
 
 // Alert displays a desktop notification and plays a default system sound.
 func Alert(title, message, appIcon string) error {
-	note := toastNotification(title, message, pathAbs(appIcon))
-	note.Audio = toast.Default
-	return note.Push()
+	if isWindows10 {
+		note := toastNotification(title, message, pathAbs(appIcon))
+		note.Audio = toast.Default
+		return note.Push()
+	}
+
+	if err := Notify(title, message, appIcon); err != nil {
+		return err
+	}
+	return Beep(DefaultFreq, DefaultDuration)
 }
